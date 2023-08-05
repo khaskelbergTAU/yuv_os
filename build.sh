7265f0917ea6d818d987e7c5735bac970a64cf8e
yuvalK <yuval@khaskelberg.org>
2023-08-05 14:40:18 +0300

    multiple args to build script allowed

--- build.sh
+++ build.sh
@@ -3,24 +3,27 @@
 QEMU=qemu-system-i386
 
 DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+while ! [[ -z $@ ]]
+do
+echo running stage $1
 case "$1" in
     "build" )
     shift 1
-    cmake --build $DIR/build $@;;
+    cmake --build $DIR/build;;
     "configure" )
     shift 1
     cd $DIR/build
     rm -rf *
-    cmake -DCMAKE_TOOLCHAIN_FILE=$DIR/toolchain.cmake -DCMAKE_CXX_COMPILER_WORKS=1 -DCMAKE_C_COMPILER_WORKS=1 $@ $DIR
+    cmake -DCMAKE_TOOLCHAIN_FILE=$DIR/toolchain.cmake -DCMAKE_CXX_COMPILER_WORKS=1 -DCMAKE_C_COMPILER_WORKS=1 $DIR
     ;;
     "run" )
     case "$2" in
         "gui" )
         shift 2
-	    $QEMU -serial stdio $@ -kernel $DIR/build/kernel/kernel.elf;;
+	    $QEMU -serial stdio -kernel $DIR/build/kernel/kernel.elf;;
         "nogui" )
         shift 2
-	    $QEMU -nographic $@ -kernel $DIR/build/kernel/kernel.elf;;
+	    $QEMU -nographic -kernel $DIR/build/kernel/kernel.elf;;
         "disk" )
         shift 2
 	    cp $DIR/build/kernel/kernel.elf $DIR/isodir/boot/kernel.elf
@@ -34,6 +37,7 @@ case "$1" in
     esac
     ;;
     "clean" )
+    shift 1
     cmake --build $DIR/build --target clean
     ;;
     *)
@@ -41,4 +45,5 @@ case "$1" in
     exit -1
     ;;
 esac
+done
 
